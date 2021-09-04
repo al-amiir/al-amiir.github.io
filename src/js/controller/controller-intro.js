aa6f05918765e5f476f7fa14cbe01f75387fe527
Elamir <engineeralamiir@gmail.com>
2021-09-04 16:54:14 +0200

    updates

--- src/js/controller/controller-intro.js
+++ src/js/controller/controller-intro.js
@@ -2,7 +2,7 @@ import { Text } from "../view/text";
 
 // Intro string array
 let introArray = [
-  "My Name is Al-Amir, a 25 y/d Front-end Web Developer, who Loves To Combine Between Programming and Art",
+  "My Name is Al-Amir, a 25 yrs Front-end Web Developer, who Loves To Combine Between Programming and Art",
   "Alexandria, Egypt",
   "",
   // "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus fugarepellendus, temporibus veniam unde neque nesciunt et",
